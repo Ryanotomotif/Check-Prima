73ee26803e59c08c3335fb65d144a691c7bafb31
Ryanotomotif <rgunawan08@student.ciputra.ac.id>
2021-12-03 10:25:07 +0700

    print "masukkan angka" dan rapikan data type

---
+++ Check Prima/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Check_Prima
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int i, angka, hitung, hasil;
+			Console.Write("Masukkan Angka : ");
+			angka = Convert.ToInt32(Console.ReadLine());
+			hitung = 0;
+			for (i = 1; i <= angka; i++)
+			{
+				hasil = angka % i;
+				if (hasil == 0)
+				hitung = hitung + 1;
+			}
+			if (hitung == 2)
+				Console.WriteLine(angka + " adalah bilangan prima");
+			else
+				Console.WriteLine(angka + " adalah bilangan bukan prima");
+		}
+	}
+}
